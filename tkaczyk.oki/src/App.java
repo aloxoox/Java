712cdef09b905be2aa4cb9241707604ef4532552
aloxoox <aleksandratkaczyk@pzsbierun.pl>
2021-02-16 17:17:11 +0100

    oki

---
+++ tkaczyk.oki/src/App.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+      Scanner scanner = new Scanner(System.in);
+      /* // WPISYWANIE IMIENIA I WYŚWIETLANIE GO
+          System.out.println("Podaj swoje imię:");
+          String firstName = scanner.nextLine();
+          System.out.println("Twoje imię to " + firstName );
+          scanner.close();
+      */
+      /* // KALKULATOR DWÓCH LICZB 
+      System.out.println("Podaj pierwszą liczbę");
+      int a = scanner.nextInt();
+      System.out.println("Podaj drugą licfzbę");      
+      int b = scanner.nextInt();
+      System.out.println(a+" + "+b+" = "+(a+b));      
+      */
+      /* // IF ELSE
+      int nr = 10;
+      if(nr == 2){
+        System.out.println("Liczba to 2");
+      }else if(nr == 3){
+        System.out.println("Liczba to 3");
+      }else{
+        System.out.println("Jakaś inna liczba niż 3 i 2");
+      }
+      // */
+
+
+    scanner.close();
+    }
+}
